40a8986c610bffdc4cce301b6aed7ba86b745c53
Rebecca Xiong <rebeccaxiong111@gmail.com>
2023-07-27 13:09:49 -0400

    Initial Commit

---
+++ github_practice/github_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_practiceApp.swift
+//  github_practice
+//
+//  Created by Scholar on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct github_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
